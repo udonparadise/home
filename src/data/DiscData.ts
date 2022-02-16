a93324e939892ea7dae48704a042646122267fcf
udonparadise <bs19.ck@gmail.com>
2022-02-16 10:57:09 +0900

    update Disc

--- src/data/DiscData.ts
+++ src/data/DiscData.ts
@@ -248,5 +248,605 @@ export const discList: disc[] = [
         doNotHave: true
       },
     ]
+  },
+  {
+    artist: Group.JUICE,
+    disc: [
+      {
+        name: 'ナルチカ2013秋 Berryz工房×Juice=Juice',
+        year: 2014
+      },
+      {
+        name: '10月10日はJuice=Juiceの日〜1st Season〜',
+        year: 2015,
+        doNotHave: true
+      },
+      {
+        name: 'Juice=Juice ファーストライブツアー2015 〜Special Juice〜',
+        year: 2015,
+        doNotHave: true
+      },
+      {
+        name: 'Juice=Juice LIVE MISSION 220 in Taipei & Hong Kong',
+        year: 2016,
+        doNotHave: true
+      },
+      {
+        name: 'Juice=Juice LIVE MISSION 220 〜Code3 Special→Growing Up！〜',
+        year: 2016,
+        doNotHave: true
+      },
+      {
+        name: 'Juice=Juice LIVE MISSION FINAL at 日本武道館',
+        year: 2017
+      },
+      {
+        name: 'Juice=Juice LIVE AROUND 2017 〜NEXT ONE SPECIAL〜',
+        year: 2017,
+        doNotHave: true
+      },
+      {
+        name: 'Juice=Juice LIVE AROUND 2017 FINAL at 日本武道館 ～Seven Squeeze!～',
+        year: 2018
+      },
+      {
+        name: 'Juice=Juice LIVE AROUND 2017～World Tour～',
+        year: 2018,
+         doNotHave: true
+      },
+      {
+        name: 'Juice=Juice LIVE GEAR 2018 ～Go ahead SPECIAL～',
+        year: 2018
+      },
+      {
+        name: 'OTODAMA SEA STUDIO 2018 ～J=J Summer Special～',
+        year: 2018,
+        doNotHave: true
+      },
+      {
+        name: 'Juice=Juice LIVE 2018 at NIPPON BUDOKAN TRIANGROOOVE',
+        year: 2019
+      },
+      {
+        name: 'Juice=Juice＆カントリー・ガールズ LIVE ～梁川奈々美 卒業スペシャル～',
+        year: 2019,
+        doNotHave: true
+      },
+      {
+        name: 'ハロプロ プレミアム Juice=Juice CONCERT TOUR 2019 ～JuiceFull!!!!!!!～ FINAL 宮崎由加卒業スペシャル',
+        year: 2019
+      },
+      {
+        name: 'OTODAMA SEA STUDIO 2019 supported by POCARI SWEAT J=J Summer Special',
+        year: 2019,
+        doNotHave: true
+      },
+      {
+        name: 'Juice=Juice Concert 2019 ～octopic!～',
+        year: 2020
+      },
+      {
+        name: 'Hello! Project presents...「Premier seat」 ～Juice=Juice Premium～',
+        year: 2021
+      },
+      {
+        name: 'Juice=Juice コンサート2020 〜続いていくSTORY〜 宮本佳林卒業スペシャル',
+        year: 2021
+      },
+      {
+        name: 'Juice=Juice 14th シングルリリース記念スペシャルライブComplete Edition.',
+        year: 2021,
+        doNotHave: true
+      },
+      {
+        name: 'Juice=Juice Concert 2021 ～FAMILIA～ 金澤朋子ファイナル',
+        year: 2022
+      },
+    ]
+  },
+  {
+    artist: 'スマイレージ&' + Group.ANGE,
+    disc: [
+      {
+        name: 'スペシャルジョイント2010春 ～感謝満開！真野恵里菜2周年突入&スマイレージ メジャーデビューへ桜咲け！ライブ～',
+        year: 2010,
+        doNotHave: true
+      },
+      {
+        name: 'スマイレージ 1stライブツアー2010秋〜デビルスマイル エンジェルスマイル〜',
+        year: 2010
+      },
+      {
+        name: 'スマイレージ 2011 Limited Live”S/mile Factory”',
+        year: 2011,
+        doNotHave: true
+      },
+      {
+        name: 'スマイレージコンサートツアー2011秋 〜逆襲の超ミニスカート〜',
+        year: 2011,
+        doNotHave: true
+      },
+      {
+        name: 'スマイレージ ベストアルバム完全版① 発売記念スペシャルコンサート',
+        year: 2012
+      },
+      {
+        name: 'スマイレージ ライブツアー2012秋〜ちょいカワ番長〜',
+        year: 2013,
+        doNotHave: true
+      },
+      {
+        name: 'スマイレージ2ndオリジナルアルバム 『②スマイルセンセーション』発売記念イベント',
+        year: 2013
+      },
+      {
+        name: 'ナルチカ2013秋 ℃-ute×スマイレージ',
+        year: 2014,
+        doNotHave: true
+      },
+      {
+        name: 'スマイレージ ライブツアー2013秋 〜スマイルチャージ〜',
+        year: 2014,
+        doNotHave: true
+      },
+      {
+        name: 'スマイレージ LIVE 2014夏 FULL CHARGE 〜715 日本武道館〜',
+        year: 2014,
+        doNotHave: true
+      },
+      {
+        name: 'スマイレージライブツアー2014秋 〜FULL CHARGE〜 FINAL in O-EAST',
+        year: 2015,
+        doNotHave: true
+      },
+      {
+        name: 'アンジュルム STARTING LIVE TOUR SPECIAL @日本武道館 『大器晩成』',
+        year: 2015,
+        doNotHave: true
+      },
+      {
+        name: 'アンジュルム ファーストコンサートツアー 2015秋 『百花繚乱』 〜福田花音卒業スペシャル〜',
+        year: 2016,
+        doNotHave: true
+      },
+      {
+        name: 'アンジュルム コンサートツアー 2016春 『九位一体』 〜田村芽実卒業スペシャル〜',
+        year: 2016
+      },
+      {
+        name: 'アンジュルム コンサートツアー 2017春 〜変わるもの 変わらないもの〜',
+        year: 2017,
+        doNotHave: true
+      },
+      {
+        name: 'OTODAMA SEA STUDIO 2017 夏だ！海だ！アンジュルムだ！！',
+        year: 2017,
+        doNotHave: true
+      },
+      {
+        name: 'アンジュルム コンサート 2017 Autumn 「Black & White」special ～風林火山～',
+        year: 2018
+      },
+      {
+        name: 'アンジュルム コンサートツアー 2018春 十人十色 ＋ ファイナル',
+        year: 20118,
+        doNotHave: true
+      },
+      {
+        name: 'ANGERME 1st Overseas Live Tour in Paris',
+        year: 2018,
+        doNotHave: true
+      },
+      {
+        name: 'ハロプロ プレミアム アンジュルム コンサートツアー 2019春 ファイナル 和田彩花卒業スペシャル 輪廻転生 ～あるとき生まれた愛の提唱～',
+        year: 2019
+      },
+      {
+        name: 'アンジュルム 2019秋「Next Page」～勝田里奈卒業スペシャル～',
+        year: 2020,
+        doNotHave: true
+      },
+      {
+        name: 'アンジュルム ライブツアー 2019夏秋「Next Page」～中西香菜卒業スペシャル～',
+        year: 2020,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project ひなフェス 2020 【アンジュルム プレミアム】',
+        year: 2020,
+        doNotHave: true
+      },
+      {
+        name: 'アンジュルム コンサート2020 〜起承転結〜 船木結卒業スペシャル',
+        year: 2021
+      },
+      {
+        name: 'アンジュルム コンサート2021「桃源郷 ～笠原桃奈 卒業スペシャル～」',
+        year: 2022
+      },
+    ]
+  },
+  {
+    artist: Group.MM,
+    disc: [
+      {
+        name: 'モーニング娘。コンサートツアー2003春NON STOP！',
+        year: 2003
+      },
+      {
+        name: 'モーニング娘。CONCERT TOUR 2003 15人でNON STOP!',
+        year: 2003
+      },
+      {
+        name: 'MORNING MUSUME。CONCERT TOUR 2004 SPRING The BEST of Japan',
+        year: 2004
+      },
+      {
+        name: "モーニング娘。コンサートツアー『The BEST of Japan 夏〜秋'04』",
+        year: 2004,
+        doNotHave: true
+      },
+      {
+        name: 'モーニング娘。コンサートツアー2005春 〜第六感 ヒット満開!〜',
+        year: 2005
+      },
+      {
+        name: 'コンサートツアー2005 夏秋『バリバリ教室～小春ちゃんいらっしゃい！～』',
+        year: 2005
+      },
+      {
+        name: 'モーニング娘。コンサートツアー2006春 ～レインボーセブン～',
+        year: 2006
+      },
+      {
+        name: 'コンサートツアー2006秋～踊れ！モーニングカレー～',
+        year: 2006
+      },
+      {
+        name: 'モーニング娘。 コンサートツアー2007春～SEXY 8 ビート～',
+        year: 2007
+      }
+    ]
+  },
+  {
+    artist: Group.HP,
+    disc: [
+      {
+        name: 'Hello! Project 2003 Winter 楽しんじゃってます!',
+        year: 2003,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2003夏～よっしゃ！ビックリサマー！！～',
+        year: 2003
+      },
+      {
+        name: "Hello! Project 2004 Winter～C'MON!ダンスワールド～",
+        year: 2004
+      },
+      {
+        name: 'Hello! Project 2004 summer～夏のド～ン！～',
+        year: 2004
+      },
+      {
+        name: 'Hello! Project 2005 Winter オールスターズ大乱舞～A HAPPY NEW POWER ! 飯田圭織 卒業スペシャル～',
+        year: 2005
+      },
+      {
+        name: "Hello! Project 2005 夏の歌謡ショー -'05 セレクション！コレクション！-",
+        year: 2005
+      },
+      {
+        name: 'Hello! Project 2006 Summer～ワンダフルハーツランド～',
+        year: 2006
+      },
+      {
+        name: 'Hello! Project 2007 Winter LIVE DVD BOX',
+        year: 2007
+      },
+      {
+        name: 'Hello! Project 2007 Summer 10th アニバーサリー大感謝祭～ハロ☆プロ夏祭り～',
+        year: 2007
+      },
+      {
+        name: 'Hello! Project 2008 Winter LIVE DVD BOX',
+        year: 2008
+      },
+      {
+        name: 'Hello! Project 2008 Summer ワンダフルハーツ公演～避暑地でデートいたしまSHOW～',
+        year: 2008
+      },
+      {
+        name: 'Hello! Project 2009 Winter ワンダフルハーツ公演 ～ 革命元年 ～/エルダークラブ公演～Thank you for your LOVE！～',
+        year: 2009
+      },
+      {
+        name: 'Hello! Project 2009 Winter 決定！ハロ☆プロ アワード ’09～エルダークラブ卒業記念スペシャル～',
+        year: 2009
+      },
+      {
+        name: 'Hello! Project 2009 SUMMER 革命元年～ Hello!チャンプル ～',
+        year: 2009
+      },
+      {
+        name: 'Hello! Project 2010 WINTER 歌超風月～シャッフルデート～',
+        year: 2010
+      },
+      {
+        name: 'Hello! Project 2010 WINTER 歌超風月～モベキマス！～',
+        year: 2010
+      },
+      {
+        name: 'Hello! Project 2010 SUMMER ～ファンコラ！～',
+        year: 2010
+      },
+      {
+        name: 'Hello! Project 2011 WINTER〜歓迎新鮮まつり〜 Aがなライブ',
+        year: 2011,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2011 WINTER〜歓迎新鮮まつり〜 Bっくりライブ',
+        year: 2011,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2011 SUMMER～ ニッポンの未来は WOW WOW YEAH YEAH ライブ ～完全版',
+        year: 2011
+      },
+      {
+        name: 'Hello! Project 2012 WINTER ハロ☆プロ天国～ロックちゃん・ファンキーちゃん～完全版',
+        year: 2012,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 誕生15周年記念ライブ 2012 夏～Wkwk夏のFAN祭り！～',
+        year: 2012,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 誕生15周年記念ライブ 2012 夏～Ktkr夏のFAN祭り！～',
+        year: 2012,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 誕生15周年記念ライブ2013冬～ビバ！・ブラボー！～完全版',
+        year: 2013
+      },
+      {
+        name: '「Hello! Project 春の大感謝 ひな祭りフェスティバル2013.3.3」完全版',
+        year: 2013
+      },
+      {
+        name: 'Hello! Project野音プレミアムLIVE 〜外フェス〜 supported by Hellosmile',
+        year: 2013,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2013 SUMMER COOL HELLO！ ～ソレゾーレ！・マゼコーゼ！～完全版',
+        year: 2013
+      },
+      {
+        name: 'Hello! Project 2014 WINTER 〜GOiSU MODE･DE-HA MiX〜完全版',
+        year: 2014
+      },
+      {
+        name: 'Hello! Project COUNTDOWN PARTY 2013 〜 GOOD BYE & HELLO ! 〜',
+        year: 2014,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project ひなフェス 2014 〜Full コース〜＜メインディッシュはモーニング娘。’14です。＞',
+        year: 2014,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project ひなフェス 2014 〜Full コース〜＜メインディッシュは℃-uteです。＞',
+        year: 2014,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2014 SUMMER 〜KOREZO！・YAPPARI！〜完全版',
+        year: 2014
+      },
+      {
+        name: 'Forest For Rest 〜里山・里海 へ行こう〜 SATOYAMA & SATOUMI with 勇気の翼 2014 収穫祭',
+        year: 2015,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project COUNTDOWN PARTY 2014 〜 GOOD BYE & HELLO ! 〜',
+        year: 2015,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2015 WINTER 〜DANCE MODE！・HAPPY EMOTION！〜完全版',
+        year: 2015
+      },
+      {
+        name: "Hello! Project ひなフェス 2015 〜 満開！The Girls' Festival 〜＜℃-ute プレミアム＞",
+        year: 2015,
+        doNotHave: true
+      },
+      {
+        name: "Hello! Project ひなフェス 2015 〜 満開！The Girls' Festival 〜＜モーニング娘。'15 プレミアム＞",
+        year: 2015,
+        doNotHave: true
+      },
+      {
+        name: "Hello! Project ひなフェス 2015 〜 満開！The Girls' Festival 〜＜アンジュルム＆Juice=Juice プレミアム＞",
+        year: 2015,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2015 SUMMER 〜DISCOVERY・CHALLENGER〜',
+        year: 2015
+      },
+      {
+        name: 'Hello! Project COUNTDOWN PARTY 2015 〜 GOOD BYE & HELLO ! 〜',
+        year: 2016,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2016 WINTER 〜 DANCING！SINGING！EXCITING！〜',
+        year: 2016
+      },
+      {
+        name: 'Hello! Project ひなフェス 2016 ＜℃-ute プレミアム＞',
+        year: 2016,
+        doNotHave: true
+      },
+      {
+        name: "Hello! Project ひなフェス 2016 ＜モーニング娘。'16 プレミアム＞",
+        year: 2016,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2016 SUMMER 〜 Sunshine Parade 〜・〜 Rainbow Carnival 〜',
+        year: 2016
+      },
+      {
+        name: 'Hello! Project COUNTDOWN PARTY 2016 〜 GOOD BYE & HELLO ! 〜',
+        year: 2017,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2017 WINTER 〜 Crystal Clear ・Kaleidoscope 〜',
+        year: 2017
+      },
+      {
+        name: 'Hello! Project ひなフェス 2017＜℃-uteプレミアム＞',
+        year: 2017,
+        doNotHave: true
+      },
+      {
+        name: "Hello! Project ひなフェス 2017＜モーニング娘。'17プレミアム＞",
+        year: 2017,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2017 SUMMER 〜 HELLO! MEETING ・ HELLO! GATHERING 〜',
+        year: 2017
+      },
+      {
+        name: 'Hello! Project 20th Anniversary!! Hello! Project COUNTDOWN PARTY 2017 ～ GOOD BYE & HELLO ! ～',
+        year: 2018,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 20th Anniversary!! Hello! Project 2018 WINTER ～PERFECT SCORE・FULL SCORE～',
+        year: 2018
+      },
+      {
+        name: "Hello! Project 20th Anniversary!! Hello! Project ひなフェス 2018 【モーニング娘。'18 プレミアム】",
+        year: 2018,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 20th Anniversary!! Hello! Project ひなフェス 2018 【Hello! Project 20th Anniversary!! プレミアム】',
+        year: 2018,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 20th Anniversary!! Hello! Project 2018 SUMMER ～ALL FOR ONE・ONE FOR ALL～',
+        year: 2018
+      },
+      {
+        name: 'Hello! Project 20th Anniversary!! Hello! Project COUNTDOWN PARTY 2018 ～ GOOD BYE & HELLO ! ～',
+        year: 2019,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 20th Anniversary!! Hello! Project 2019 WINTER ～YOU & I・NEW AGE～',
+        year: 2019,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 20th Anniversary!! Hello! Project ひなフェス 2019 【Hello! Project 20th Anniversary!! プレミアム】',
+        year: 2019,
+        doNotHave: true
+      },
+      {
+        name: "Hello! Project 20th Anniversary!! Hello! Project ひなフェス 2019 【モーニング娘。'19 プレミアム】",
+        year: 2019,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2019 SUMMER 「beautiful/harmony」',
+        year: 2019
+      },
+      {
+        name: 'Hello! Project COUNTDOWN PARTY 2019 ～ GOOD BYE & HELLO ! ～',
+        year: 2020,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2020 Winter HELLO! PROJECT IS [　　　　　] ～side A / side B～',
+        year: 2020
+      },
+      {
+        name: 'Hello! Project ひなフェス 2020 【アンジュルム プレミアム】',
+        year: 2020,
+        doNotHave: true
+      },
+      {
+        name: "Hello! Project ひなフェス 2020 【モーニング娘。'20 プレミアム】",
+        year: 2020,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project presents...「ソロフェス！」',
+        year: 2020,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2020 ～The Ballad～ Special Number',
+        year: 2021,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project presents...「Premier seat」～Hello! Project Premium～',
+        year: 2021,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2020 COVERS ～The Ballad Best Selection～',
+        year: 2021,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project Year-End Party 2020 ～GOOD BYE & HELLO ! ～',
+        year: 2021,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2021 Winter ～STEP BY STEP～',
+        year: 2021,
+        doNotHave: true
+      },
+      {
+        name: "Hello! Project ひなフェス 2021【モーニング娘。'21/アンジュルム プレミアム】",
+        year: 2021,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project ひなフェス 2021【Juice=Juice/つばきファクトリー＆BEYOOOOONDS プレミアム】',
+        year: 2021,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project presents...「ソロフェス！2」',
+        year: 2021
+      },
+      {
+        name: 'Hello! Project 2021 Summer Sapphire & Ruby',
+        year: 2021,
+        doNotHave: true
+      },
+      {
+        name: 'Hello! Project 2021 秋 「続・花鳥風月」',
+        year: 2022
+      },
+    ]
   }
 ];
