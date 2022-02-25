03c00389445637e019c5bb26b26fdd9b8d73ddce
udonparadise <bs19.ck@gmail.com>
2022-02-25 18:06:56 +0900

    add Note

--- src/data/NoteData.ts
+++ src/data/NoteData.ts
@@ -2,6 +2,10 @@ import { note } from '../model/Note';
 
 export const notes: note[] = [
   {
+    date: new Date(2022, 2, 25),
+    note: 'どんと来い! BE HAPPY! 行きてー！'
+  },
+  {
     date: new Date(2022, 2, 17),
     note: '久々にハロコンのBDを買った（続・花鳥風月）¥n三郷収録とは珍しい'
   },
