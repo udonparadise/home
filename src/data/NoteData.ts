302deeebd0a274daafa914dfe1e39c5e209c96f8
udonparadise <bs19.ck@gmail.com>
2022-03-03 15:30:05 +0900

    fix Note

--- src/data/NoteData.ts
+++ src/data/NoteData.ts
@@ -2,23 +2,23 @@ import { note } from '../model/Note';
 
 export const notes: note[] = [
   {
-    date: new Date(2022, 3, 3),
+    date: new Date(2022, 2, 3),
     note: 'terzo プラプラ収録されている!? 優勝！'
   },
   {
-    date: new Date(2022, 3, 1),
+    date: new Date(2022, 2, 1),
     note: '9期・10期よりも14期が先に卒業するなんて…'
   },
   {
-    date: new Date(2022, 2, 28),
+    date: new Date(2022, 1, 28),
     note: '早くみいみの特大アクスタ欲しいよ¥n10月まで待てないよ'
   },
   {
-    date: new Date(2022, 2, 25),
+    date: new Date(2022, 1, 25),
     note: 'どんと来い! BE HAPPY! 行きてー！'
   },
   {
-    date: new Date(2022, 2, 17),
+    date: new Date(2022, 1, 17),
     note: '久々にハロコンのBDを買った（続・花鳥風月）¥n三郷収録とは珍しい'
   }
 ];
